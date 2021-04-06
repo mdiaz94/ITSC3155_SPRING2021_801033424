ea5b17a6065460a6a947255ccf83007a8b898896
Michael Diaz <mdiaz11@uncc.edu>
2021-04-06 01:18:50 -0400

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,6 +7,10 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for 
 from database import db
+from models import Note as Note
+from models import User as User
+
+
 
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
@@ -32,9 +36,12 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+@app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
+    #get user from database
+    a_user =  db.session.query(User).filter_by(email='mogli@uncc.edu')
+    #a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
 
     return render_template('index.html', user=a_user)
 
@@ -43,7 +50,13 @@ def index():
 
 @app.route("/notes")
 def get_notes():
-    a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
+    #retrieve user from database
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    #retrieve notes from database
+    my_notes = db.session.query(Note).all()
+    
+    
+    #a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
 
     return render_template('notes.html', notes=notes, user=a_user)
 
@@ -51,7 +64,13 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
+    #retrieve user from database
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    #retrieve note from database
+    my_note = db.session.query(Note).filter_by(id=note_id)
+    
+    
+    #a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
 
     return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
@@ -60,8 +79,7 @@ def get_note(note_id):
 @app.route('/notes/new', methods = ['GET','POST'])
 def new_note():
     # create a mock user
-    a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
-
+    #a_user = {'name': 'Michael Diaz', 'email':'mogli@uncc.edu'} #Mock
     #check method used for request
     if request.method == 'POST':
         #get title data
@@ -74,13 +92,13 @@ def new_note():
 
         #formate date mm/dd/yyyy
         today = today.strftime("%m-%d-%Y")
-        #get the lad ID used and increment by 1
-        id = len(notes)+1
-        #create new note entry
-        notes[id] = {'title': title, 'text':text, 'date':today}
+        newEntry = Note(title, text, today)
+        db.session.add(newEntry)
+        db.session.commit()
 
         return redirect(url_for('get_notes', name = a_user))
     else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
         return render_template ('new.html', user=a_user)
 
 
